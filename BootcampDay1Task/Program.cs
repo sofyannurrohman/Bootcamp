fe07995e47d8915c9500edd2c2f2ab2a762859c4
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-09-09 13:15:16 +0700

    feat: implement FizzBuzz console application- Add number printing from 1 to n with divisibility rules- Support foo (divisible by 3), bar (divisible by 5), foobar (divisible by both)- Include proper formatting with comma separators- Add user input for flexible n value

---
+++ BootcampDay1Task/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Capture User Input
+Console.Write("Enter a number (n): ");
+int n = int.Parse(Console.ReadLine());
+//Foo bar IMPL
+for (int x = 1; x <= n; x++)
+{
+    if (x % 15 == 0)
+        Console.Write("foobar");
+    else if (x % 3 == 0)
+        Console.Write("foo");
+    else if (x % 5 == 0)
+        Console.Write("bar");
+    else
+        Console.Write(x);
+    if (x < n)
+        Console.Write(", ");
+}
+//Write Output
+Console.WriteLine();
--- BootcampDay2/Program.cs
+++ BootcampDay2/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string name = "";
-name = Console.ReadLine();
-Console.WriteLine("Hello, " + name);
