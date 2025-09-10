3801f26c3ca5be3238913d69beb05b5e97f4f1d0
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-09-10 08:24:49 +0700

    Additional gender statement

--- BootcampDay1/Entities/User.cs
+++ BootcampDay1/Entities/User.cs
@@ -5,5 +5,6 @@ public class User
     public string firstName { get; set; } = "";
     public string lastName { get; set; } = "";
     public DateTime BirthDate { get; set; }
-    
+    public string gender { get; set; } = "Unknown Gender";
+
 }
--- BootcampDay1/Program.cs
+++ BootcampDay1/Program.cs
@@ -26,7 +26,8 @@ class Program
 
         Console.Write("Please enter your last name: ");
         user.lastName = Console.ReadLine()?.Trim() ?? "";
-
+        Console.Write("Please enter your gender male / female: ");
+        user.gender = Console.ReadLine()?.Trim() ?? "";
         Console.Write("Please enter your birth date (yyyy-MM-dd): ");
         if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
         {
@@ -34,15 +35,15 @@ class Program
         }
         else
         {
-            user.BirthDate = DateTime.MinValue; // fallback if invalid
+            user.BirthDate = DateTime.MinValue; 
         }
-
         return user;
     }
 
     static void GreetUser(User user)
     {
         string fullName = $"{user.firstName} {user.lastName}".Trim();
+        string gender = user.gender.ToLower();
         if (string.IsNullOrWhiteSpace(fullName))
         {
             Console.WriteLine("Hello, stranger!");
@@ -51,7 +52,14 @@ class Program
         {
             Console.WriteLine($"Hello, {fullName}!");
         }
-
+        if (gender == "male")
+        {
+            Console.WriteLine($"Hello Dude !");
+        }
+        else
+        {
+            Console.WriteLine($"Hello girl !");
+        }
         if (user.BirthDate != DateTime.MinValue)
         {
             Console.WriteLine($"Your birth date is: {user.BirthDate:yyyy-MM-dd}");
