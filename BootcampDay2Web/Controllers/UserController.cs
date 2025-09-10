d4cc637f10a4f8b5c1af53435c552ed87ea1dcea
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-09-10 09:26:33 +0700

    feat: creating UserController and View

--- BootcampDay2Web/Controllers/UserController.cs
+++ BootcampDay2Web/Controllers/UserController.cs
@@ -1,4 +1,6 @@
+using Dtos.UserRegisterDto;
 using Entities.User;
+using Interfaces.IUserAuth;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,36 @@ namespace Controllers.UserController
 {
     public class UserController : Controller
     {
-        private List<User> _users = new List<User>();
-
+        private readonly IUserAuth _userService;
+        public UserController(IUserAuth userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserRegisterDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                bool success = _userService.Register(dto);
+                if (success)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email Already Registered ");
+                }
+            }
+            return View("Login");
+        }
+        public IActionResult Welcome(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
     }
 }
--- BootcampDay2Web/Program.cs
+++ BootcampDay2Web/Program.cs
@@ -1,29 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Interfaces.IUserAuth;
+using Services.UserService;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IUserAuth, UserService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // replace MapStaticAssets if not custom
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=User}/{action=Register}/{id?}");
 
 app.Run();
