6a418171d5348c1252804a8176cbe302549547e5
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-09-11 09:38:34 +0700

    feat: Create Class Product and Test Impl

---
+++ BootcampDay3Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using Classes;
+
+//Create Product 1
+Product product1 = new Product();
+product1.Name = "Laptop";
+product1.Price = 899.99f;
+
+// Show product info
+product1.DisplayInfo();
+
+Console.WriteLine();
+
+// Create Product 2 with name constructor
+Product product2 = new Product("Smartphone");
+product2.Price = 499.50f;
+
+Console.WriteLine(product2.ToString());
+
+Console.ReadLine();
