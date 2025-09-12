153513fddc232604c80c2afc6b1130e531edae40
sofyannurrohman <sofyannurrohman1@gmail.com>
2025-09-12 10:10:45 +0700

    refractor: Adding personal note for development

--- BootcampDay3InterfaceApi/Program.cs
+++ BootcampDay3InterfaceApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // DI implementation for IUserRepository with InMemoryUserRepository
-builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>(); //Whenever something asks for IUserRepository, give them a UserRepository
 
 var app = builder.Build();
 
@@ -14,3 +14,18 @@ var app = builder.Build();
 app.MapControllers();
 
 app.Run();
+
+/* Personal Note :
+Flow to create of this Api:
+Define a contract / interface in IUserRepository
+            |
+            v
+Define a class for implementation of contract in UserRepository (ex: comunicate with db creating crud)
+            |
+            v
+Controller will Dependency Inject the Contract and use what it need instead inherit of contract
+            |
+            v
+Program.cs Will handle the registration of IUserRepository and UserRepository
+
+*/
